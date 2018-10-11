def362fae7c6ab8962501af88670f88c80c8b6fa
John Buckler <jjbuckler@gmail.com>
2018-10-11 15:55:33 -0400

    adding bootstrap elements

--- map4x/js/main.js
+++ map4x/js/main.js
@@ -1,9 +1,11 @@
 require([
   'esri/Map',
   'esri/views/MapView',
+  "esri/layers/FeatureLayer",
   // Bootstrap
   'bootstrap/Collapse',
   'bootstrap/Dropdown',
+
 
   // Calcite Maps
   'calcite-maps/calcitemaps-v0.9',
@@ -14,18 +16,42 @@ require([
 ], function (
   Map,
   MapView,
+  FeatureLayer,
   Collapse,
   Dropdown,
   CalciteMaps,
   CalciteMapArcGISSupport
 ) {
+
+  let app = [];
+
   var map = new Map({
     basemap: 'streets'
   })
   var view = new MapView({
     container: 'viewDiv', // Reference to the scene div created in step 5
     map: map, // Reference to the map object created before the scene
-    zoom: 4, // Sets zoom level based on level of detail (LOD)
-    center: [15, 65] // Sets center point of view using longitude,latitude
+    extent: {
+      xmin: -2500000,
+      ymin: 150000,
+      xmax: 2400000,
+      ymax: 3900000,
+      spatialReference: {
+        wkid: 102039
+      }
+    },
+    padding: {
+      top: 50,
+      bottom: 0
+    },
+
+    zoom: 5, // Sets zoom level based on level of detail (LOD)
+    center: [-98, 39] // Sets center point of view using longitude,latitude
   })
+  var featureLayer = new FeatureLayer({
+    url: "https://gis.ers.usda.gov/arcgis/rest/services/ra_people/MapServer/0"
+
+  });
+  featureLayer.opacity = .8;
+  map.add(featureLayer);
 })
